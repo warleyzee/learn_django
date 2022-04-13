c5076f469458cac83543749a5dc5ba26f64fb55a
warleyzee <warleyzee@hotmail.com>
2022-04-13 08:24:16 +0100

    finish

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from recipes.views import about, home,contact
+# from recipes.views import about, home,contact
 
 
 
--- recipes/views.py
+++ recipes/views.py
@@ -5,10 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html')
 
 def contact(request):
-    return HttpResponse('Contacts')
+    return render(request, 'recipes/contact.html')
 
 def about(request):
-    return HttpResponse('About')
+    return HttpResponse('About') 
